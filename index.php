b405dfaea86de27a1f6af0ba7492bffff85630fd
Cezar Leigos <cezarleigos28@outlook.com>
2021-04-09 19:45:26 -0300

    v0.3 - Melhorando Interface
    Dessa vez nós definimos uma pouco melhor a interface do nosso questionário, atualizamos o arquivo index.php e o arquivo style.css onde fizemos a maior parte das alterações.

--- index.php
+++ index.php
@@ -6,56 +6,58 @@
 </head>
 <body>
     
+    <div class="menu">QUESTIONARIO.COM</div>
     <div class="questionario">
     	<div class="questionario-grupo">
-    		<span class="questionario-numero-questao">1</span>
-    		 - 
-    		<span class="questionario-texto-questao"> É aqui onde o texto da pergunta vai ficar </span>
-    	</div>
-    	<div class="questionario-item">
-    		<div class="questionario-opcao">
-    			<div class="questionario-opcao-letra">A</div>
-    			<input type="radio" name="questao1" class="questionario-opcao-radio">
-    			<div class="questionario-opcao-texto">Uma das opções do questionario</div>
-    			<div class="questionario-opcao-resposta">&check;</div>
-    		</div>
-        </div>	
+            <span class="questionario-numero-questao">1</span>- 
+            <span class="questionario-texto-questao"> É aqui onde o texto da pergunta vai ficar </span>
+        </div>
         <div class="questionario-item">
-    		<div class="questionario-opcao">
-    			<div class="questionario-opcao-letra">B</div>
-    			<input type="radio" name="questao1" class="questionario-opcao-radio">
-    			<div class="questionario-opcao-texto">Uma das opções do questionario</div>
-    			<div class="questionario-opcao-resposta">&cross;</div>
-    		</div>
-        </div>	
+           <div class="questionario-opcao">
+               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1a">
+               <div class="questionario-opcao-letra">A - </div>
+               <div class="questionario-opcao-texto">Uma das opções do questionario</div>
+               <div class="questionario-opcao-resposta">&check;</div>
+            </div>
+        </div>
         <div class="questionario-item">
-    		<div class="questionario-opcao">
-    			<div class="questionario-opcao-letra">C</div>
-    			<input type="radio" name="questao1" class="questionario-opcao-radio">
-    			<div class="questionario-opcao-texto">Uma das opções do questionario</div>
-    			<div class="questionario-opcao-resposta">&cross;</div>
-    		</div>
-        </div>	
+           <div class="questionario-opcao">
+               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1b">
+               <div class="questionario-opcao-letra">B - </div>
+               <div class="questionario-opcao-texto">Uma das opções do questionario</div>
+               <div class="questionario-opcao-resposta">&check;</div>
+            </div>
+        </div>
         <div class="questionario-item">
-    		<div class="questionario-opcao">
-    			<div class="questionario-opcao-letra">D</div>
-    			<input type="radio" name="questao1" class="questionario-opcao-radio">
-    			<div class="questionario-opcao-texto">Uma das opções do questionario</div>
-    			<div class="questionario-opcao-resposta">&cross;</div>
-    		</div>
-        </div>	
+           <div class="questionario-opcao">
+               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1c">
+               <div class="questionario-opcao-letra">C - </div>
+               <div class="questionario-opcao-texto">Uma das opções do questionario</div>
+               <div class="questionario-opcao-resposta">&check;</div>
+            </div>
+        </div>
         <div class="questionario-item">
-    		<div class="questionario-opcao">
-    			<div class="questionario-opcao-letra">E</div>
-    			<input type="radio" name="questao1" class="questionario-opcao-radio">
-    			<div class="questionario-opcao-texto">Uma das opções do questionario</div>
-    			<div class="questionario-opcao-resposta">&cross;</div>
-    		</div>
+           <div class="questionario-opcao">
+               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1d">
+               <div class="questionario-opcao-letra">D - </div>
+               <div class="questionario-opcao-texto">Uma das opções do questionario</div>
+               <div class="questionario-opcao-resposta">&check;</div>
+            </div>
+        </div>
+        <div class="questionario-item">
+           <div class="questionario-opcao">
+               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1e">
+               <div class="questionario-opcao-letra">E - </div>
+               <div class="questionario-opcao-texto">Uma das opções do questionario</div>
+               <div class="questionario-opcao-resposta">&check;</div>
+            </div>
         </div>	
+
+
         <button class="questionario-submit" type="submit">Finalizar Questionario</button>
     </div>	
 
-    <script type="text/javascript" src="app/js/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="app/js/script.js"></script>
+ <script type="text/javascript" src="app/js/jquery-3.6.0.min.js"></script>
+ <script type="text/javascript" src="app/js/script.js"></script>
 </body>
 </html>
