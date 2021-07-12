6a658a5e4735153db071456c0076885e13c05714
Cezar Leigos <cezarleigos28@outlook.com>
2021-07-12 20:48:58 -0300

    v0.4 - Começando a criar perguntas e respostas
    Dessa vez nós começamos a criar as perguntas e respostas que ficarão de exemplo para que possamos testar nosso questionário,

---
+++ app/php/modulo-questionario.php
@@ -0,0 +1,26 @@
+<?php
+
+$perguntas = [
+	"Quantos dias tem uma semana?",
+	"Que dia é comemorado o natal?",
+	"Quando começou e em que ano terminou a primeira guerra mundial?",
+	"Qual é o primeiro mês do ano?",
+	"Qual é o último mês do ano?",
+	""
+];
+
+$respostas = [
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	""
+];
+
+
+?>
