e4773de3fd343281550a7aa6702594df0e5a480b
Paulo <cezarleigos28@outlook.com>
2021-07-13 16:25:50 -0300

    v0.5 - Finalizando layoult
    Dessa vez nós finalizamos o layoult do questionário para que possamos aplicar os eventos em javascript

--- app/php/modulo-questionario.php
+++ app/php/modulo-questionario.php
@@ -1,25 +1,29 @@
 <?php
 
 $perguntas = [
-	"Quantos dias tem uma semana?",
-	"Que dia é comemorado o natal?",
-	"Quando começou e em que ano terminou a primeira guerra mundial?",
-	"Qual é o primeiro mês do ano?",
-	"Qual é o último mês do ano?",
-	""
+	"Quantos dias tem uma semana?", //1
+	"Que dia é comemorado o natal?", //2
+	"Quando começou e em que ano terminou a primeira guerra mundial?", //3
+	"Qual é o primeiro mês do ano?", //4
+	"Qual é o último mês do ano?", //5
+	"Como se diz 'vermelho' em inglês?", //6
+	"Quantos dias tem um ano?", //7
+	"Quantos estados tem o Brasil?", //8
+	"Quem descobriu o Brasil?", //9
+	"Que dia se comemora a independência do brasil" //10
 ];
 
 $respostas = [
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	""
+	"7", //1
+	"25 de dezembro", //2
+	"2014-2018", //3
+	"Janeiro", //4
+	"Dezembro", //5
+	"Vermelho", //6
+	"365", //7
+	"26 estados + distrito federal", //8
+	"Pedro Álvares Cabral", //9
+	"7 de Setembro" //10
 ];
 
 
--- index.php
+++ index.php
@@ -5,16 +5,69 @@
 	<link rel="stylesheet" type="text/css" href="app/css/style.css">
 </head>
 <body>
+
+  <?php include("app/php/modulo-questionario.php"); ?>
+
+  <?php
+     
+    echo "
+         <script>
+
+             var pergunta_1 = '".$perguntas[0]."';
+             var resposta_1 = '".$respostas[0]."';
+
+             var pergunta_2 = '".$perguntas[1]."';
+             var resposta_2 = '".$respostas[1]."';
+
+             var pergunta_3 = '".$perguntas[2]."';
+             var resposta_3 = '".$respostas[2]."';
+
+             var pergunta_4 = '".$perguntas[3]."';
+             var resposta_4 = '".$respostas[3]."';
+
+             var pergunta_5 = '".$perguntas[4]."';
+             var resposta_5 = '".$respostas[4]."';
+
+             var pergunta_6 = '".$perguntas[5]."';
+             var resposta_6 = '".$respostas[5]."';
+
+             var pergunta_7 = '".$perguntas[6]."';
+             var resposta_7 = '".$respostas[6]."';
+
+             var pergunta_8 = '".$perguntas[7]."';
+             var resposta_8 = '".$respostas[7]."';
+
+             var pergunta_9 = '".$perguntas[8]."';
+             var resposta_9 = '".$respostas[8]."';
+
+             var pergunta_10 = '".$perguntas[9]."';
+             var resposta_10 = '".$respostas[9]."';
+            
+
+         </script>
+    ";
+
+  ?>
     
     <div class="menu">QUESTIONARIO.COM</div>
+
+
     <div class="questionario">
-    	<div class="questionario-grupo">
-            <span class="questionario-numero-questao">1</span>- 
-            <span class="questionario-texto-questao"> É aqui onde o texto da pergunta vai ficar </span>
+
+      <?php
+         
+        for($i = 0; $i < 10; $i++){
+
+      ?>
+
+      <div class="questionario-bloco">
+    	  <div class="questionario-grupo">
+            <span class="questionario-numero-questao"><?php echo $i + 1; ?> -</span> 
+            <span class="questionario-texto-questao"> <?php echo $perguntas[$i]; ?> </span>
         </div>
         <div class="questionario-item">
            <div class="questionario-opcao">
-               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1a">
+               <input type="radio" name="questao<?php echo $i + 1; ?>" class="questionario-opcao-radio" value="q_1a">
                <div class="questionario-opcao-letra">A - </div>
                <div class="questionario-opcao-texto">Uma das opções do questionario</div>
                <div class="questionario-opcao-resposta">&check;</div>
@@ -22,7 +75,7 @@
         </div>
         <div class="questionario-item">
            <div class="questionario-opcao">
-               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1b">
+               <input type="radio" name="questao<?php echo $i + 1; ?>" class="questionario-opcao-radio" value="q_1b">
                <div class="questionario-opcao-letra">B - </div>
                <div class="questionario-opcao-texto">Uma das opções do questionario</div>
                <div class="questionario-opcao-resposta">&check;</div>
@@ -30,7 +83,7 @@
         </div>
         <div class="questionario-item">
            <div class="questionario-opcao">
-               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1c">
+               <input type="radio" name="questao<?php echo $i + 1; ?>" class="questionario-opcao-radio" value="q_1c">
                <div class="questionario-opcao-letra">C - </div>
                <div class="questionario-opcao-texto">Uma das opções do questionario</div>
                <div class="questionario-opcao-resposta">&check;</div>
@@ -38,7 +91,7 @@
         </div>
         <div class="questionario-item">
            <div class="questionario-opcao">
-               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1d">
+               <input type="radio" name="questao<?php echo $i + 1; ?>" class="questionario-opcao-radio" value="q_1d">
                <div class="questionario-opcao-letra">D - </div>
                <div class="questionario-opcao-texto">Uma das opções do questionario</div>
                <div class="questionario-opcao-resposta">&check;</div>
@@ -46,13 +99,19 @@
         </div>
         <div class="questionario-item">
            <div class="questionario-opcao">
-               <input type="radio" name="questao1" class="questionario-opcao-radio" value="q_1e">
+               <input type="radio" name="questao<?php echo $i + 1; ?>" class="questionario-opcao-radio" value="q_1e">
                <div class="questionario-opcao-letra">E - </div>
                <div class="questionario-opcao-texto">Uma das opções do questionario</div>
                <div class="questionario-opcao-resposta">&check;</div>
             </div>
         </div>	
+      </div>
 
+      <?php
+
+      }
+
+      ?>
 
         <button class="questionario-submit" type="submit">Finalizar Questionario</button>
     </div>	
